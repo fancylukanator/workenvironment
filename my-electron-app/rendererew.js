75a7f7b7aa7c02fdaeb6841261531d5954adea47
Owen Kent <owen5031@gmail.com>
2021-06-29 22:12:30 -0400

    Delete rendererew.js
    This shouldn't have been there...

--- my-electron-app/rendererew.js
+++
@@ -1,8 +0,0 @@
-const ipc = require('electron').ipcRenderer,
-
-// Checks for when the launch button is clicked
-var button = document.getElementById('launch')
-button.addEventListener('click',function(){
-    console.log("OK...")
-    ipc.send('test')
-})
