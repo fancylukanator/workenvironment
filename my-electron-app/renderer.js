529bcd87471cdecbceb754f2b33feae47c74c750
Owen Kent <owen5031@gmail.com>
2021-06-30 09:56:18 -0400

    (Minor) improvements to project creation
    Added ability to select multiple links, files, and applications. List used to keep track of everything associated with project.

--- my-electron-app/renderer.js
+++ my-electron-app/renderer.js
@@ -2,7 +2,36 @@ const electron = require('electron');
 const ipc = electron.ipcRenderer;
 
 // Checks for when the launch button is clicked
-var button = document.getElementById('launch')
-button.addEventListener('click',function(){
+document.getElementById('launch').addEventListener('click',function(){
     ipc.send('launch')
 })
+
+// Updates list of links
+document.getElementById('link').addEventListener('change', (event) => {
+    var input = document.getElementById("link").value; 
+    var child = "<li>" + input + "</li>";
+    document.getElementById("linkList").innerHTML += '<ul>'+child+'<ul>';
+    document.getElementById("link").value = ""; // clear the value
+  });
+
+// Updates list of files
+document.getElementById('addfile').addEventListener('change', (event) => {
+    var input = document.getElementById('addfile');
+    var output = document.getElementById('fileList');
+    var children = "";
+    for (var i = 0; i < input.files.length; ++i) {
+        children += '<li>' + input.files.item(i).name + '</li>';
+    }
+    output.innerHTML += '<ul>'+children+'</ul>';
+  });
+
+// Updates list of applications
+document.getElementById('addapp').addEventListener('change', (event) => {
+    var input = document.getElementById('addapp');
+    var output = document.getElementById('appList');
+    var children = "";
+    for (var i = 0; i < input.files.length; ++i) {
+        children += '<li>' + input.files.item(i).name + '</li>';
+    }
+    output.innerHTML += '<ul>'+children+'</ul>';
+  });
