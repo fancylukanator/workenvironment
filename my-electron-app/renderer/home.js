3444771caea95a0ea7084e511d6393fbb702bc22
fancylukanator <lukesteenge@gmail.com>
2021-07-01 14:01:18 -0400

    displaying project data in window on the right

--- my-electron-app/renderer/home.js
+++ my-electron-app/renderer/home.js
@@ -9,11 +9,35 @@ document.getElementById('launch').addEventListener('click',function(){
 });
 
 // create a list element for each project in localStorage
-var list = document.getElementById("projectList");
+var projectList = document.getElementById("projectList");
 projectData = JSON.parse(localStorage.MyProjectList);
 for(var i in projectData) {
     var entry = document.createElement('li');
-    entry.setAttribute('onclick', console.log("hey"));
+    entry.onclick = displayProject;
     entry.appendChild(document.createTextNode(projectData[i].name));
-    list.appendChild(entry);
+    projectList.appendChild(entry);
+}
+
+// function that displays all information about the project
+function displayProject () {
+    document.getElementById("projectName").textContent += projectData[i].name;
+    for(var j in projectData[i].urls){
+        var linkList = document.getElementById("projectLinks");
+        var linkEntry = document.createElement('li');
+        linkEntry.appendChild(document.createTextNode(projectData[i].urls[j]));
+        linkList.appendChild(linkEntry);
+    }
+    for(var k in projectData[i].files) {
+        var fileList = document.getElementById("projectFiles");
+        var fileEntry = document.createElement('li');
+        fileEntry.appendChild(document.createTextNode(projectData[i].files[k]));
+        fileList.appendChild(fileEntry);
+    }
+    for(var l in projectData[i].apps) {
+        var appList = document.getElementById("projectApps");
+        var appEntry = document.createElement('li');
+        appEntry.appendChild(document.createTextNode(projectData[i].apps[l]));
+        appList.appendChild(appEntry);
+    }
+    document.getElementById("display_project").style.display = "block";
 }
