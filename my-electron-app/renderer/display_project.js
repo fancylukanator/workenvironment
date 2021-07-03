943b296ce07212899d40da307b82a83e33ba94c1
Owen Kent <owen5031@gmail.com>
2021-07-03 09:37:02 -0400

    Added ability to hide project previews
    If the user clicks again on a project that is already being previewed, the project is hidden. Changed word 'links' to be 'urls'.

--- my-electron-app/renderer/display_project.js
+++ my-electron-app/renderer/display_project.js
@@ -1,41 +1,48 @@
 // function that displays all information about the project
 function displayProject(index) {
 
-    // Get project data of interest
-    projectData = JSON.parse(localStorage.MyProjectList)[index];
-
-    // Set the name of the project and its index in storage
-    document.getElementById("projectName").textContent = projectData.name;
-    document.getElementById("projectName").index = index
-
-    // List out all of the URLs
-    document.getElementById("projectURLs").innerHTML = "";
-    for(var j in projectData.urls){
-        var linkList = document.getElementById("projectURLs");
-        var linkEntry = document.createElement('li');
-        linkEntry.appendChild(document.createTextNode(projectData.urls[j]));
-        linkList.appendChild(linkEntry);
+    // If the user is already previewing the project they clicked on... hide the project
+    if(document.getElementById("projectName").index == index && document.getElementById("display_project").style.display == "block"){
+        document.getElementById("display_project").style.display = "none";
     }
+    // If the user wants to view a new project...
+    else{
+        // Get project data of interest
+        projectData = JSON.parse(localStorage.MyProjectList)[index];
 
-    // List out all of the files
-    document.getElementById("projectFiles").innerHTML = "";
-    for(var k in projectData.files) {
-        var fileList = document.getElementById("projectFiles");
-        var fileEntry = document.createElement('li');
-        fileEntry.appendChild(document.createTextNode(projectData.files[k]));
-        fileList.appendChild(fileEntry);
-    }
+        // Set the name of the project and its index in storage
+        document.getElementById("projectName").textContent = projectData.name;
+        document.getElementById("projectName").index = index
 
-    // List all of the applications
-    document.getElementById("projectApps").innerHTML = "";
-    for(var l in projectData.apps) {
-        var appList = document.getElementById("projectApps");
-        var appEntry = document.createElement('li');
-        appEntry.appendChild(document.createTextNode(projectData.apps[l]));
-        appList.appendChild(appEntry);
+        // List out all of the URLs
+        document.getElementById("projectURLs").innerHTML = "";
+        for(var j in projectData.urls){
+            var linkList = document.getElementById("projectURLs");
+            var linkEntry = document.createElement('li');
+            linkEntry.appendChild(document.createTextNode(projectData.urls[j]));
+            linkList.appendChild(linkEntry);
+        }
+
+        // List out all of the files
+        document.getElementById("projectFiles").innerHTML = "";
+        for(var k in projectData.files) {
+            var fileList = document.getElementById("projectFiles");
+            var fileEntry = document.createElement('li');
+            fileEntry.appendChild(document.createTextNode(projectData.files[k]));
+            fileList.appendChild(fileEntry);
+        }
+
+        // List all of the applications
+        document.getElementById("projectApps").innerHTML = "";
+        for(var l in projectData.apps) {
+            var appList = document.getElementById("projectApps");
+            var appEntry = document.createElement('li');
+            appEntry.appendChild(document.createTextNode(projectData.apps[l]));
+            appList.appendChild(appEntry);
+        }
+        document.getElementById("display_project").style.display = "block";
+        document.getElementById("create_project").style.display = "none";
     }
-    document.getElementById("display_project").style.display = "block";
-    document.getElementById("create_project").style.display = "none";
 }
 
 
