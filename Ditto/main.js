71a092d50e9556e874b2bb67b5e003b0c0241a29
Luke Steenge <lukesteenge@Karins-MacBook-Pro.local>
2021-08-06 15:38:15 -0400

    dropdown menu configure, now have to give it functionality

--- Ditto/main.js
+++ Ditto/main.js
@@ -3,7 +3,7 @@
 // Modules to control application life and create native browser window
 const electron = require('electron');
 const path = require('path');
-const { app, BrowserWindow, shell, ipcMain} = require('electron');
+const { app, BrowserWindow, shell, ipcMain, Menu, Tray, nativeImage} = require('electron');
 const ipc = ipcMain;
 
 
@@ -28,11 +28,48 @@ function createWindow () {
   mainWindow.webContents.openDevTools()
 }
 
+/*app.on('ready', () => {
+  tray = new Tray(path.join(__dirname,'Icon.png'));
+
+  if (process.platform === 'win32') {
+    tray.on('click', tray.popUpContextMenu);
+  }
+
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Quit',
+      click() { app.quit(); }
+    }
+  ]);
+
+  tray.setToolTip('Ditto');
+  tray.setContextMenu(menu);
+});*/
+
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+let tray
+
 app.whenReady().then(() => {
+
+  const icon = nativeImage.createFromPath('icon@2x.png')
+  tray = new Tray(icon)
+
   
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2 but looooonger', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ])
+  
+  tray.setContextMenu(contextMenu)
+
+  tray.setToolTip('Ditto')
+
     createWindow()
 
     app.on('activate', function () {
@@ -40,7 +77,8 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
+
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -48,7 +86,6 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
-
 
 
 
