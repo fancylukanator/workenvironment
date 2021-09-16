c3242cf6aea92ae8b27e5400d1a80fbf8e384cd7
Owen Kent <owen5031@gmail.com>
2021-09-16 09:40:11 -0400

    Update TrayIcon.js

--- Ditto/tray/TrayIcon.js
+++ Ditto/tray/TrayIcon.js
@@ -1,40 +1,38 @@
 const path = require('path');
-const { BrowserWindow, Tray, ipcMain } = require('electron');
-const { webContents } = require('electron')
-
-// Electron-positioner - npm package for positioning of the Tray window. Our tray window should appear under the Tray icon.
-
+const {webContents, BrowserWindow, Tray, ipcMain } = require('electron');
 const Positioner = require('electron-positioner');
 
 class TrayIcon {
   constructor(trayWindow) {
+
     // Path to the app icon that will be displayed in the Tray (icon size: 18px)
     let iconPath = path.join(__dirname, './../pictures/menubarlogo@2x.png')
 
+    // Create new tray icon
     this.trayIcon = new Tray(iconPath);
-    //this.trayIcon.setToolTip('ditto'); // This tooltip will shows up, when user will hover our tray-icon.
 
-    // By clicking on the icon we have to show Tray Window and position it in the middle under the tray icon. ( Initialy this window is hidden )
-
+    // By clicking on the icon we have to show Tray Window and position it in the middle under the tray icon.
     this.trayIcon.on('click', (e, bounds) => {
+
+      // If tray is already open...
       if ( trayWindow.isVisible() ) {
         trayWindow.hide();
-        // Added to make the menubar icon compatible with multiple desktops
-        trayWindow.setVisibleOnAllWorkspaces(true);
-        
+        trayWindow.setVisibleOnAllWorkspaces(true); // Added to make the menubar icon compatible with multiple desktops
+
+      // If tray is not open...
       } else {
         let positioner = new Positioner(trayWindow);
         positioner.move('trayCenter', bounds)
 
-        // send notice to page to run functions to display most up to date data
+        // Send notice to page to run functions to display most up to date data
         trayWindow.webContents.send('ping', 'opened')
 
+        // Display tray window
         trayWindow.show();
         trayWindow.focus();
 
         // Causes the menubar window to only be visible on the desktop it is opened on (mimic native menubar windows)
         trayWindow.setVisibleOnAllWorkspaces(false);
-
       }
     });
 
@@ -42,6 +40,8 @@ class TrayIcon {
 
     // Dynamic menubar height
     ipcMain.on('menubar-height', function(event,args) {
+
+      // Used to store the height
       let height;
 
       // If a project is open and more than 1 workspace has been created
@@ -64,12 +64,13 @@ class TrayIcon {
         height = 88 + 26*args[0];
       }
 
+      // Set the window size
       trayWindow.setSize(275,height);
     });
   }
 
 
-  
+  // Updates the title of the tray
   updateTitle(title) {
     this.trayIcon.setTitle(title)
   }
